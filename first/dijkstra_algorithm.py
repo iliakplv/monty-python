b20e6b246c667c0304911a61cd18d5f1958f8261
iliakplv <rtKathf08>
2014-02-21 00:06:05 +0400

    dijkstra

---
+++ first/dijkstra_algorithm.py
@@ -0,0 +1,54 @@
+inf = 10000
+
+
+# graph
+
+adjacency = {
+    '1-2': 7,
+    '1-3': 9,
+    '1-6': 14,
+    '2-3': 10,
+    '2-4': 15,
+    '3-4': 11,
+    '3-6': 2,
+    '4-5': 6,
+    '5-6': 9
+}
+
+vertexes_to_visit = {1, 2, 3, 4, 5, 6}
+
+paths = {1: '', 2: '', 3: '', 4: '', 5: '', 6: ''}
+
+paths_lengths = {1: inf, 2: inf, 3: inf, 4: inf, 5: inf, 6: inf}
+
+
+# start and finish vertexes
+
+start = 1
+
+
+# initialization
+
+current_vertex = start
+paths[current_vertex] = str(current_vertex)
+paths_lengths[current_vertex] = 0
+
+
+# TODO algorithm
+
+# while vertexes_to_visit not empty
+
+    # delete current_vertex from vertexes_to_visit
+
+    # for vertex in vertexes_to_visit and adjacent
+        # temp_path_length = current_vertex path_length + adjacency_value
+        # if (temp_path_length < existing path to vertex)
+            # init vertex path length (temp_path_length)
+            # init vertex path ('current_vertex path' + 'vertex')
+
+    # set current_vertex to vertex: adjacent, with minimal path_length, not visited (if existing)
+
+print('\nresults\n')
+for vertex in paths.keys():
+    print(vertex, paths_lengths[vertex], paths[vertex], sep='\t\t')
+
