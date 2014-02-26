9fe8fb86b3394cdf0e48fae4967f938b4201de29
iliakplv <rtKathf08>
2014-02-27 00:39:18 +0400

    fix

--- first/dijkstra_algorithm.py
+++ first/dijkstra_algorithm.py
@@ -1,4 +1,5 @@
 inf = 10000
+delimiter = '-'
 
 
 # Graph definition
@@ -34,47 +35,46 @@ vertexes_to_visit.remove(current_vertex)
 while vertexes_to_visit:
 
     # visiting adjacent vertexes
+
     for vertex_in_visit in vertexes_to_visit:
 
         # creating string path
         if current_vertex < vertex_in_visit:
-            path_to_vertex_from_current = str(current_vertex) + '-' + str(vertex_in_visit)
+            path_to_vertex_from_current = str(current_vertex) + delimiter + str(vertex_in_visit)
         else:
-            path_to_vertex_from_current = str(vertex_in_visit) + ' ' + str(current_vertex)
+            path_to_vertex_from_current = str(vertex_in_visit) + delimiter + str(current_vertex)
 
         # calculating minimal path to visited vertex
         if path_to_vertex_from_current in adjacency:
             temp_path_length = paths_lengths[current_vertex] + adjacency[path_to_vertex_from_current]
             if temp_path_length < paths_lengths[vertex_in_visit]:
                 paths_lengths[vertex_in_visit] = temp_path_length
-                paths[vertex_in_visit] = paths[current_vertex] + '-' + str(vertex_in_visit)
-
+                paths[vertex_in_visit] = paths[current_vertex] + delimiter + str(vertex_in_visit)
 
     # searching for next current vertex in not visited vertexes
 
     min_path_length = inf + 1
-    new_current_vertex = ''
+    next_current_vertex = ''
 
     for vertex in vertexes_to_visit:
 
         # creating string path
         if current_vertex < vertex:
-            path_to_vertex_from_current = str(current_vertex) + '-' + str(vertex)
+            path_to_vertex_from_current = str(current_vertex) + delimiter + str(vertex)
         else:
-            path_to_vertex_from_current = str(vertex) + '-' + str(current_vertex)
+            path_to_vertex_from_current = str(vertex) + delimiter + str(current_vertex)
 
         # searching for adjacent vertex with minimal path
         if path_to_vertex_from_current in adjacency:
             if paths_lengths[vertex] < min_path_length:
                 min_path_length = paths_lengths[vertex]
-                new_current_vertex = vertex
+                next_current_vertex = vertex
 
-    if new_current_vertex == '':
+    if next_current_vertex == '':
         break
     else:
-        current_vertex = new_current_vertex
+        current_vertex = next_current_vertex
         vertexes_to_visit.remove(current_vertex)
-
 
 
 # Result
@@ -82,4 +82,3 @@ while vertexes_to_visit:
 print('\nResults\n[vertex | path length | path]\n')
 for vertex_in_visit in paths.keys():
     print(vertex_in_visit, paths_lengths[vertex_in_visit], paths[vertex_in_visit], sep='\t\t')
-
