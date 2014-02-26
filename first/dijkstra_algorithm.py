55edb71d601217f5252598d38ca6ca0fb6aab19a
iliakplv <iliakplv@gmail.com>
2014-02-26 18:21:50 +0400

    algorithm [todo]

--- first/dijkstra_algorithm.py
+++ first/dijkstra_algorithm.py
@@ -50,23 +50,34 @@ paths_lengths[current_vertex] = 0
 while vertexes_to_visit:
     vertexes_to_visit.remove(current_vertex)
 
-    current_shortest_path = '-?'
-    current_shortest_path_length = inf
+    for vertex_in_visit in vertexes_to_visit:
+        if vertex_in_visit != current_vertex: # (removed)
+            path_to_vertex_from_current = str(current_vertex) + '-' + str(vertex_in_visit)
+            if path_to_vertex_from_current in adjacency:
+                temp_path_length = paths_lengths[current_vertex] + adjacency[path_to_vertex_from_current]
+                if temp_path_length < paths_lengths[vertex_in_visit]:
+                    paths_lengths[vertex_in_visit] = temp_path_length
+                    paths[vertex_in_visit] = paths[current_vertex] + '-' + str(vertex_in_visit)
+
+    min_path_length = inf
+    new_current_vertex = ''
 
     for vertex in vertexes_to_visit:
         path_to_vertex_from_current = str(current_vertex) + '-' + str(vertex)
-
         if path_to_vertex_from_current in adjacency:
-            if path_to_vertex_from_current < current_shortest_path_length:
-                current_shortest_path = '-' + str(vertex)
-                current_shortest_path_length = path_to_vertex_from_current
+            if paths_lengths[vertex] < min_path_length:
+                min_path_length = paths_lengths[vertex]
+                new_current_vertex = vertex
 
-
+    if new_current_vertex == '':
+        break
+    else:
+        current_vertex = new_current_vertex
 
 
 # Result
 
 print('\nResults\n[vertex | path length | path]\n')
-for vertex in paths.keys():
-    print(vertex, paths_lengths[vertex], paths[vertex], sep='\t\t')
+for vertex_in_visit in paths.keys():
+    print(vertex_in_visit, paths_lengths[vertex_in_visit], paths[vertex_in_visit], sep='\t\t')
 
