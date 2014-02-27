6bbaaa922c28f86ef5908df47c69ea7a83dc4490
iliakplv <rtKathf08>
2014-02-27 21:28:25 +0400

    debug

--- first/dijkstra_algorithm.py
+++ first/dijkstra_algorithm.py
@@ -1,4 +1,5 @@
 inf = 10000
+no = -1
 
 
 # Graph definition
@@ -11,7 +12,7 @@ adjacency = [[inf, 7, 9, inf, inf, 14],
              [14, inf, 2, inf, 9, inf]]
 
 vertexes_to_visit = {0, 1, 2, 3, 4, 5}  # initially - all vertexes
-previous_vertexes = {0: '', 1: '', 2: '', 3: '', 4: '', 5: ''}
+previous_vertexes = {0: no, 1: no, 2: no, 3: no, 4: no, 5: no}
 paths_lengths = {0: inf, 1: inf, 2: inf, 3: inf, 4: inf, 5: inf}
 
 
@@ -20,7 +21,6 @@ paths_lengths = {0: inf, 1: inf, 2: inf, 3: inf, 4: inf, 5: inf}
 start_vertex = 0
 
 current_vertex = start_vertex
-previous_vertexes[current_vertex] = -1
 paths_lengths[current_vertex] = 0
 vertexes_to_visit.remove(current_vertex)
 
@@ -31,12 +31,12 @@ while vertexes_to_visit:
 
     # visiting adjacent vertexes
 
-    for vertex_in_visit in paths_lengths.keys():
-        if adjacency[current_vertex][vertex_in_visit] < inf:
-            temp_path_length = paths_lengths[current_vertex] + adjacency[current_vertex][vertex_in_visit]
-            if temp_path_length < paths_lengths[vertex_in_visit]:
-                paths_lengths[vertex_in_visit] = temp_path_length
-                previous_vertexes[vertex_in_visit] = current_vertex
+    for vertex in paths_lengths.keys():
+        if adjacency[current_vertex][vertex] < inf:
+            temp_path_length = paths_lengths[current_vertex] + adjacency[current_vertex][vertex]
+            if temp_path_length < paths_lengths[vertex]:
+                paths_lengths[vertex] = temp_path_length
+                previous_vertexes[vertex] = current_vertex
 
     # searching for next current vertex in not visited vertexes
 
@@ -49,6 +49,12 @@ while vertexes_to_visit:
                 min_path_length = paths_lengths[vertex]
                 next_current_vertex = vertex
 
+    # todo debug
+    # print('\ncurrent = {cv} \n'.format(cv=current_vertex+1))
+    # for vertex in paths_lengths.keys():
+    #     print(vertex+1, paths_lengths[vertex], previous_vertexes[vertex]+1, sep='\t\t')
+
+
     if next_current_vertex == '':
         break
     else:
@@ -56,8 +62,8 @@ while vertexes_to_visit:
         vertexes_to_visit.remove(current_vertex)
 
 
-# Result # todo
+# Result
 
-print('\nResults\n[vertex | path length | previous]\n')
+print('\nResult\n[vertex | path length | previous]\n')
 for vertex in paths_lengths.keys():
-    print(vertex+1, paths_lengths[vertex], previous_vertexes[vertex]+1, sep='\t\t')
+    print(vertex, paths_lengths[vertex], previous_vertexes[vertex], sep='\t\t')
