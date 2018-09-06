310008028a2ae9e533dc5ce649298156b01764cc
Ilia Kopylov <iliakplv@gmail.com>
2018-09-06 15:00:35 +1200

    Use normal distribution of coordinates

--- src/drawing/plot.py
+++ src/drawing/plot.py
@@ -1,14 +1,23 @@
 from random import randint
+import numpy as np
 import matplotlib.pyplot as plt
 
 # plt.gca().invert_yaxis()
+plt.axis('equal')
 
-stroke_count = 800
-canvas_size = 500
+stroke_count = 100
+canvas_size = 1000
+mean = 0
+std = 0.8
+
+
+def rand():
+    return np.random.normal(mean, std, 2)
+
 
 for i in range(stroke_count):
-    x_series = [randint(0, canvas_size), randint(0, canvas_size)]
-    y_series = [randint(0, canvas_size), randint(0, canvas_size)]
-    plt.plot(x_series, y_series)
+    x_series = rand()
+    y_series = rand()
+    plt.plot(x_series, y_series, linewidth=randint(10, 20))
 
 plt.show()
