5be2f0edc37b1b2c5570871956c8242f7f27193f
Ilia Kopylov <iliakplv@gmail.com>
2018-09-06 15:24:58 +1200

    Combine normal and uniform based drawing

--- src/drawing/plot.py
+++ src/drawing/plot.py
@@ -6,19 +6,34 @@ import matplotlib.pyplot as plt
 plt.axis('equal')
 plt.axis('off')
 
-stroke_count = 100
-canvas_size = 1000
+stroke_count_u = 1
+stroke_points_u = 400
+
+stroke_count_n = 100
+stroke_points_n = 2
+
 mean = 0
-std = 0.8
+std = 1.0
 
 
-def rand():
-    return np.random.normal(mean, std, 2)
+# Normal
+def rand_n():
+    return np.random.normal(mean, std, size=stroke_points_n)
 
 
-for i in range(stroke_count):
-    x_series = rand()
-    y_series = rand()
+# Uniform
+def rand_u():
+    return np.random.uniform(low=-3.0, high=3.0, size=stroke_points_u)
+
+
+for i in range(stroke_count_u):
+    x_series = rand_u()
+    y_series = rand_u()
+    plt.plot(x_series, y_series, linewidth=2, color='k')
+
+for i in range(stroke_count_n):
+    x_series = rand_n()
+    y_series = rand_n()
     plt.plot(x_series, y_series, linewidth=randint(10, 20))
 
 plt.show()
