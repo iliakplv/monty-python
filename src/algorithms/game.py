2d095f94a609fa0798dfdff4194ec86e6e9bbda1
Ilia Kopylov <ilia@springload.co.nz>
2020-02-05 15:39:37 +1300

    Make number of games per iteration random

--- src/algorithms/game.py
+++ src/algorithms/game.py
@@ -1,10 +1,13 @@
 from random import random
+from random import randint
 
 # Simulation of a random game with variable win ratio
 
 prizes_left = 3000
 iterations = 10
-games_per_iter = 1000  # TODO this might be variable
+games_per_iter_min = 800
+games_per_iter_max = 1200
+games_per_iter_predicted = 1000
 win_ratio = 1  # updated every iteration
 
 
@@ -14,13 +17,14 @@ def play():
 
 for iter in range(0, iterations):
     iterations_left = iterations - iter
-    win_ratio = prizes_left / (games_per_iter * iterations_left)
+    win_ratio = prizes_left / (games_per_iter_predicted * iterations_left)
     prizes_won = 0
+    games = randint(games_per_iter_min, games_per_iter_max)
 
-    for game in range(0, games_per_iter):
+    for game in range(0, games):
         game_won = play()
         if game_won:
             prizes_won += 1
             prizes_left -= 1
 
-    print(f"Iter: {iter}, Prizes won: {prizes_won}, Prizes left: {prizes_left}, \tWin ratio: {win_ratio}")
+    print(f"Iter: {iter:2} | Games: {games:4} | Prizes won: {prizes_won:3} | Prizes left: {prizes_left:4} | Win ratio: {win_ratio}")
